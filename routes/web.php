e2a624f6c7aaf79a300491981aa553d13552feba
加藤 <wabisabiwasabi430@gmail.com>
2020-12-04 07:23:08 +0000

    php 12宿題

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 
@@ -25,6 +25,15 @@ Route::get('XXX','admin\AAAController@bbb');
 
 
 
-Route::get('admin/profile/create','admin/ProfileController@add');
+Route::get('admin/profile/create','admin/ProfileController@add')->middleware('auth');
 
-Route::get('admin/profile/edit','Admin\ProfileController@edit');
+Route::get('admin/profile/edit','Admin\ProfileController@edit')->middleware('auth');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
