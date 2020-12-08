23eddd8f7fa65b36a9c8a880e998e2b9af163851
加藤 <wabisabiwasabi430@gmail.com>
2020-12-08 07:36:23 +0000

    Laravel 13課題

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); 
+     Route::post('news/create', 'Admin\ProfileController@create');
+     Route::post('news/create','Admin\ProfileController@update');
 });
 
 
